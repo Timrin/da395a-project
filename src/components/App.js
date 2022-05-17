87f898c657819458a4263671f76e4acc165725dd
Timrin <19280360+Timrin@users.noreply.github.com>
2022-05-17 23:59:25 +0200

    Implement a basic display for a poem
    Implemented a PoemContainer, it displays author, title, and text of a poem.
    Currently using dummy data for the poem

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import PoemContainer from './PoemContainer';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <PoemContainer />
     </div>
   );
 }
