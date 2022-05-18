1a359dfb20920db4c55f7b8386a11d8657e3366f
Simon-Mategen <simon.mategen@gmail.com>
2022-05-18 11:56:14 +0200

    Can now write poem to local storage

--- src/localStorage_util.js
+++ src/localStorage_util.js
@@ -7,6 +7,16 @@ export function getPoem(){
 }
 
 function writePoemToLocalStorage(inPoem){
+    const poems = readPoemsFromLocalStorage();
+
+    poems.push({
+        title:inPoem[0].title,
+        author:inPoem[0].author,
+        lines:inPoem[0].lines
+    });
+
+    let jsonPoems = JSON.stringify(poems);
+    localStorage.setItem("poems", jsonPoems);
 
 }
 
