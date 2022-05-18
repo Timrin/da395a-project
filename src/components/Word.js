960ac5807c841b98b2aa648601811fdb3151f0be
Hanna Ringkvist <ringkvist.hanna@gmail.com>
2022-05-18 09:39:08 +0200

    Lagt till definitionen av ett specifikt ord i en Modal med hjälp av Bootstrap

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,14 +1,25 @@
-import React from "react";
+
+import React, {useState, useEffect} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Modal, Button} from 'react-bootstrap';
+
+
 
 export default function Word() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-  const testWord = "Hello";
+  const [definition, setDefinition] = useState([]);
+
+  const testWord = "Bottle";
 
   const fetchWord = () => {
     fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + testWord)
     .then(res => res.json())
     .then(
       (result) => {
+        setDefinition(result[0].meanings[0].definitions[0].definition)
         console.log(result[0].meanings[0].definitions[0].definition)
       }, (error) => {
         console.log(error)
@@ -16,9 +27,33 @@ export default function Word() {
     )
   }
 
+
+  useEffect(() => {
+    fetchWord()
+}, [])
+
+
   return (
     <div>
-      <button onClick={() => {fetchWord()}}>Hej</button>
+      <Button className="nextButton" onClick={handleShow}>
+        A specific word
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Definition of {testWord}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{definition}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Word
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
     </div>
   )
 
