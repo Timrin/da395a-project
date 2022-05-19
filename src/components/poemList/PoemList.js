3be6905ec431c5ad0a42a3cb719888f41f079b2e
Simon-Mategen <simon.mategen@gmail.com>
2022-05-19 20:29:44 +0200

    Created PoemListItem.js

--- src/components/poemList/PoemList.js
+++ src/components/poemList/PoemList.js
@@ -1,4 +1,5 @@
 import './poemList_style.css'
+import PoemListItem from './PoemListItem'
 
 export default function PoemList(){
     return(
@@ -6,7 +7,7 @@ export default function PoemList(){
             <h3>Sparade dikter</h3>
             <hr />
             <ul>
-
+                <PoemListItem />
             </ul>
         </div>
     )
---
+++ src/components/poemList/PoemListItem.js
@@ -0,0 +1,7 @@
+export default function PoemListItem() {
+    return(
+        <li className="list-group-item">
+            <p>Item</p>
+        </li>
+    )
+}
