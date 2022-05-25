28b1d0b96b8e525f7626d8a533f99098785acaa7
Simon-Mategen <simon.mategen@gmail.com>
2022-05-25 09:38:08 +0200

    Removed console log

--- src/components/poemList/PoemListItem.js
+++ src/components/poemList/PoemListItem.js
@@ -6,7 +6,6 @@ export default function PoemListItem(props) {
         <ListGroup.Item>
             <p>{props.item.title} | {props.item.author} <span className='delete' onClick={() => {
                 props.deletePoemById(props.item.id);
-                console.log("Delete CLick")
             }}>X</span></p>
             <hr />
         </ListGroup.Item>
