75626715e67ba7210e0ec526b5aa49d9dc092ec7
Simon-Mategen <simon.mategen@gmail.com>
2022-05-29 17:45:44 +0200

    Fixed styling after bug fix

--- src/components/poemList/WordListItem.js
+++ src/components/poemList/WordListItem.js
@@ -8,32 +8,35 @@ export default function WordListItem(props) {
     const MAX_NUMBER_OF_WORD_DEFINITIONS = 2;
 
     return(
-        <OverlayTrigger rootClose trigger="click" placement="top" overlay={
-            <Popover className="popoverWrap" placement="top" id="popover-basic" show={false}>
-                <Popover.Header as="h4">Defnition of {props.item.word}</Popover.Header>
-                <Popover.Body>
-                    {props.item.phonetic}
-                    {props.item.meanings.map(meaning => {
-                        let definitions = [];
-                        //Only loop for MAX_NUMBER_OF_WORD_DEFINITIONS or less 
-                        let loop = meaning.definitions.length > MAX_NUMBER_OF_WORD_DEFINITIONS ? MAX_NUMBER_OF_WORD_DEFINITIONS : meaning.definitions.length
-                        for (let i = 0; i < loop; i++) {
-                            definitions.push(meaning.definitions[i])
-                        }
-                        return (
-                            <div>
-                                <h5>{meaning.partOfSpeech}</h5>
-                                <ul>
-                                    {definitions.map((definition, index) => <li key={index}>{definition.definition}</li>)}
-                                </ul>
-                            </div>
-                        )
-                    })}
-                </Popover.Body>
-            </Popover>}>
             <ListGroup.Item className='word-box-item'>
-                <p className='word'><strong>{props.item.word}</strong></p> 
-                <p className='phonetic'> [{props.item.phonetic}] </p>
+                <OverlayTrigger rootClose trigger="click" placement="top" overlay={
+                    <Popover className="popoverWrap" placement="top" id="popover-basic" show={false}>
+                        <Popover.Header as="h4">Defnition of {props.item.word}</Popover.Header>
+                        <Popover.Body>
+                            {props.item.phonetic}
+                            {props.item.meanings.map(meaning => {
+                                let definitions = [];
+                                //Only loop for MAX_NUMBER_OF_WORD_DEFINITIONS or less 
+                                let loop = meaning.definitions.length > MAX_NUMBER_OF_WORD_DEFINITIONS ? MAX_NUMBER_OF_WORD_DEFINITIONS : meaning.definitions.length
+                                for (let i = 0; i < loop; i++) {
+                                    definitions.push(meaning.definitions[i])
+                                }
+                                return (
+                                    <div>
+                                        <h5>{meaning.partOfSpeech}</h5>
+                                        <ul>
+                                            {definitions.map((definition, index) => <li key={index}>{definition.definition}</li>)}
+                                        </ul>
+                                    </div>
+                                )
+                            })}
+                        </Popover.Body>
+                    </Popover>}>
+                    <div className='word-header-container'>
+                        <p className='word'><strong>{props.item.word}</strong></p> 
+                        <p className='phonetic'> [{props.item.phonetic}] </p>
+                    </div>
+                </OverlayTrigger>
                 
                 <div className='word-delete-container'>
                     <span className='word-delete' onClick={() => {
@@ -42,8 +45,6 @@ export default function WordListItem(props) {
                             <img src={logo} />
                     </span>
                 </div>
-
             </ListGroup.Item>
-        </OverlayTrigger>
-    )
+    );
 }
