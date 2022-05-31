9c03c851d5676fe219f49098127686dc12d4acd4
Timrin <19280360+Timrin@users.noreply.github.com>
2022-05-31 16:46:52 +0200

    Add Floating action bar navigation menu for mobile

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import PoemContainer from './PoemContainer';
 import { writePoemToLocalStorage, writeWordToLocalStorage, deleteFromLocalStorage } from "../localStorage_util";
 import Dropdown from 'react-bootstrap/Dropdown';
 import { getNewPoem, getWord } from "./api_util";
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap';
+import './FloatingActionButton.css';
 
 function App() {
 
@@ -37,38 +38,60 @@ function App() {
     deleteFromLocalStorage('words', id);
   }
 
-  return (
-    <div className="App">
-      <div id="menuBar">
-        
-        <img className="logo" src={'quill.png'} />
-
-        <div className="menu-btn">
-          <Button className="btn" onClick={() => { getNewPoem(setPoem, setIsLoaded) }}>New Poem</Button>
-          <Button className="btn" onClick={() => { savePoem(poem) }}><span className="material-icons">turned_in</span></Button>
-        </div>
-
-        <div className="mobile-show">
-
-        </div>
-      
+  return ([<div className="fab-container mobile-show">
+    <div className="fab shadow">
+      <div className="fab-content">
+        <span className="material-icons">menu</span>
       </div>
-      <div id="header">
-        <h1>Dicta</h1>
-      </div>
-
-      <div className="list-container">
-        <div id="poemList">
-          <List type="poems" deletePoem={deletePoem} setPoem={setPoem} setIsLoaded={setIsLoaded} />
-        </div>
-        <div id="wordList">
-          <List type="words" deleteWord={deleteWord} />
-        </div>
-      </div>
-      
-      <PoemContainer savePoem={savePoem} saveWord={saveWord} poem={poem} setPoem={setPoem}
-       isLoaded={isLoaded} setIsLoaded={setIsLoaded}/>
     </div>
+    <div className="sub-button shadow">
+      <a href="#poemList">
+        <span className="material-icons">bookmarks</span>
+      </a>
+    </div>
+    <div className="sub-button shadow">
+      <a href="#wordList">
+        <span className="material-icons">star</span>
+      </a>
+    </div>
+    <div className="sub-button shadow">
+      <a href="#top">
+        <span className="material-icons">arrow_upward</span>
+      </a>
+    </div>
+  </div>,
+  <div className="App">
+
+    <div id="menuBar">
+
+      <img className="logo" src={'quill.png'} />
+
+      <div className="menu-btn">
+        <Button className="btn" onClick={() => { getNewPoem(setPoem, setIsLoaded) }}>New Poem</Button>
+        <Button className="btn" onClick={() => { savePoem(poem) }}><span className="material-icons">turned_in</span></Button>
+      </div>
+
+      <div className="mobile-show">
+      <h1>Dicta</h1>
+      </div>
+
+    </div>
+    <div id="header">
+      <h1>Dicta</h1>
+    </div>
+
+    <div className="list-container">
+      <div id="poemList">
+        <List type="poems" deletePoem={deletePoem} setPoem={setPoem} setIsLoaded={setIsLoaded} />
+      </div>
+      <div id="wordList">
+        <List type="words" deleteWord={deleteWord} />
+      </div>
+    </div>
+
+    <PoemContainer savePoem={savePoem} saveWord={saveWord} poem={poem} setPoem={setPoem}
+      isLoaded={isLoaded} setIsLoaded={setIsLoaded} />
+  </div>]
   );
 }
 
